e508ffebb09410b0498579b3624bc9c66ad43cb9
Rosangely De Los Santos <192157716+rosangely-dls@users.noreply.github.com>
2026-03-07 17:32:41 -0500

    lesson 14 work

--- app.cjs
+++ app.cjs
@@ -1,6 +1,14 @@
 require("dotenv").config();
 
 const express = require("express");
+
+// --- Security imports ---
+const cookieParser = require("cookie-parser");
+const csrf = require("csurf");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+
 const session = require("express-session");
 const flash = require("connect-flash");
 const MongoDBStore = require("connect-mongodb-session")(session);
@@ -13,13 +21,37 @@ require("./passport/passportInit")();
 
 const app = express();
 
+// --- Security middleware ---
+app.use(cookieParser(process.env.SESSION_SECRET));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(helmet());
+app.use(xss());
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
+
+app.use(limiter);
+
 const Book = require("./models/Book");
+
+function isAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+
+  req.flash("error", "You must log in first.");
+  res.redirect("/");
+}
 
 // --- View Engine ---
 app.set("view engine", "ejs");
 
 // --- Middleware ---
-app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(express.static("public"));
 
 // --- MongoDB connection ---
@@ -54,6 +86,14 @@ if (app.get("env") === "production") {
 
 app.use(session(sessionParams));
 
+// --- CSRF protection ---
+app.use(csrf());
+
+app.use((req, res, next) => {
+  res.locals._csrf = req.csrfToken();
+  next();
+});
+
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -71,7 +111,6 @@ app.use(require("./middleware/storeLocals"));
 
 // --- Routes ---
 app.use("/sessions", require("./routes/sessionRoutes"));
-app.use("/secretWord", require("./routes/secretWord"));
 
 // Home page
 app.get("/", (req, res) => {
@@ -79,8 +118,8 @@ app.get("/", (req, res) => {
 });
 
 // --- GET /books ---
-app.get("/books", async (req, res) => {
-  const books = await Book.find();
+app.get("/books", isAuthenticated, async (req, res) => {
+  const books = await Book.find({ createdBy: req.user._id });
   res.render("books", { books });
 });
 
@@ -101,6 +140,7 @@ app.post("/books", async (req, res) => {
       rating,
       review,
       bannedReason,
+      createdBy: req.user._id,
     });
 
     req.flash("info", "Book added successfully!");
@@ -109,6 +149,54 @@ app.post("/books", async (req, res) => {
     req.flash("error", "Something went wrong!");
     res.redirect("/books");
   }
+});
+
+// --- GET edit book ---
+app.get("/books/edit/:id", isAuthenticated, async (req, res) => {
+  const books = await Book.findOne({
+    _id: req.params.id,
+    createdBy: req.user._id,
+  });
+
+  if (!books) {
+    req.flash("error", "Book not found.");
+    return res.redirect("/books");
+  }
+
+  res.render("books", { books });
+});
+
+// --- POST update book ---
+app.post("/books/update/:id", isAuthenticated, async (req, res) => {
+  const { title, author, genre, rating, review, bannedReason } = req.body;
+
+  await Book.findByIdAndUpdate(
+    { _id: req.params.id, createdBy: req.user._id },
+    {
+      title,
+      author,
+      genre,
+      rating,
+      review,
+      bannedReason,
+    },
+  );
+
+  req.flash("info", "Book updated!");
+
+  res.redirect("/books");
+});
+
+// --- POST delete book ---
+app.post("/books/delete/:id", isAuthenticated, async (req, res) => {
+  await Book.findByIdAndDelete({
+    _id: req.params.id,
+    createdBy: req.user._id,
+  });
+
+  req.flash("info", "Book deleted!");
+
+  res.redirect("/books");
 });
 
 // --- 404 ---
